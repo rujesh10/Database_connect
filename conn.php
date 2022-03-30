29bf84e5001085d6a2ce8a3f69c8b04de3b7a489
rujesh10 <91970415+rujesh10@users.noreply.github.com>
2022-03-30 11:41:34 +0545

    Add files via upload

---
+++ conn.php
@@ -0,0 +1,20 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "practice";
+
+// connection 
+
+$con = mysqli_connect("$host","$username","$password","$dbname");
+
+// connection check
+
+// if(!$con){
+//     echo ("error connecting to the data base!");
+// }
+// else{
+//     echo("Data base connection success");
+// }
+
+// ?>
